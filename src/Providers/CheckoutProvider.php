5ef3d652ce27f4546b7c55ea637227023f3f867f
Sebastian Engel-Wolf <sebastian@mail-wolf.de>
2020-08-12 11:36:24 +0200

    add config for QR Code on confirmation

--- src/Providers/CheckoutProvider.php
+++ src/Providers/CheckoutProvider.php
@@ -11,6 +11,7 @@ use Plenty\Modules\Order\Pdf\Events\OrderPdfGenerationEvent;
 use Plenty\Modules\Order\Pdf\Models\OrderPdfGeneration;
 use Plenty\Modules\Document\Models\Document;
 use KeepoalaWidget2\Services\Logger;
+use KeepoalaWidget2\Services\Config;
 use Ceres\Config\CeresHeaderConfig;
 
 class CheckoutProvider extends ServiceProvider
@@ -24,59 +25,67 @@ class CheckoutProvider extends ServiceProvider
  
 	}
  
-	public function boot(Twig $twig, Dispatcher $eventDispatcher, Logger $logger)
+	public function boot(Twig $twig, Dispatcher $eventDispatcher, Logger $logger, Config $config)
     {
         $eventDispatcher->listen('IO.Resources.Import', function (ResourceContainer $container)
         {
             $container->addScriptTemplate('KeepoalaWidget2::Content.Scripts');
         }, 0);
 
-        $this->registerInvoicePdfGeneration($eventDispatcher, $logger);
+        $this->registerInvoicePdfGeneration($eventDispatcher, $logger, $config);
     }
 
     private function registerInvoicePdfGeneration(
         Dispatcher $eventDispatcher,
-        Logger $logger
+        Logger $logger,
+        Config $config
     ) {
-        // Listen for the document generation event
-        $eventDispatcher->listen(OrderPdfGenerationEvent::class,
-            function (OrderPdfGenerationEvent $event) use ($logger) {
 
-                /** @var Order $order */
-                $order = $event->getOrder();
-                $docType = $event->getDocType();
+        if ($config->get("global.qrorder")) {
+            // Listen for the document generation event
+            $eventDispatcher->listen(OrderPdfGenerationEvent::class,
+                function (OrderPdfGenerationEvent $event) use ($logger) {
 
-                $shop_config = pluginApp(CeresHeaderConfig::class);
-                $company_name = $shop_config->companyName;
+                    /** @var Order $order */
+                    $order = $event->getOrder();
+                    $docType = $event->getDocType();
 
-                $keepoalaID = substr(md5($company_name), 0, 4) . '-' .  md5($order->id);
-                $link = "https://www.keepoala.com/enter-code/?code=" . $keepoalaID;
-                if ($docType == Document::INVOICE) {
+                    $shop_config = pluginApp(CeresHeaderConfig::class);
+                    $company_name = $shop_config->companyName;
 
-                    try {
-                        $orderPdfGenerationModel = pluginApp(OrderPdfGeneration::class);
+                    $keepoalaID = substr(md5($company_name), 0, 4) . '-' .  md5($order->id);
+                    $link = "https://www.keepoala.com/enter-code/?code=" . $keepoalaID;
+                    if ($docType == Document::ORDER_CONFIRMATION) {
+
+                        try {
+                            $orderPdfGenerationModel = pluginApp(OrderPdfGeneration::class);
+                            $orderPdfGenerationModel->language = 'de';
+                            $orderPdfGenerationModel->image = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=".$link;
+                            $orderPdfGenerationModel->imageHeight = 120;
+                            $orderPdfGenerationModel->imageWidth = 120;
+                            $orderPdfGenerationModel->link = $link;
+
+                            $orderPdfGenerationModelAdvice = pluginApp(OrderPdfGeneration::class);
+                            $orderPdfGenerationModelAdvice->language = "de";
+                            $orderPdfGenerationModelAdvice->advice = "TestText";
+
+                        } catch (\Exception $e) {
+                            $message = "KeepoalaWidget2" . '::' . 'Adding PDF comment failed for order '
+                                . $order->id;
+                            $logger->error($message, $e->getMessage());
+                            return;
+                        }
+                        /** @var \Plenty\Modules\Order\Pdf\Models\OrderPdfGeneration $orderPdfGenerationModel */
+                        /** $orderPdfGenerationModel = pluginApp(OrderPdfGeneration::class);
+
                         $orderPdfGenerationModel->language = 'de';
-                        $orderPdfGenerationModel->image = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=".$link;
-                        $orderPdfGenerationModel->imageHeight = 120;
-                        $orderPdfGenerationModel->imageWidth = 120;
-                        $orderPdfGenerationModel->link = $link;
 
-                    } catch (\Exception $e) {
-                        $message = "KeepoalaWidget2" . '::' . 'Adding PDF comment failed for order '
-                            . $order->id;
-                        $logger->error($message, $e->getMessage());
-                        return;
+                        **/
+                        /* ADD IMAGE */
+                        $event->addOrderPdfGeneration($orderPdfGenerationModel)->addOrderPdfGeneration($orderPdfGenerationModelAdvice);
                     }
-                    /** @var \Plenty\Modules\Order\Pdf\Models\OrderPdfGeneration $orderPdfGenerationModel */
-                    /** $orderPdfGenerationModel = pluginApp(OrderPdfGeneration::class);
-
-                    $orderPdfGenerationModel->language = 'de';
-
-                    **/
-                    /* ADD IMAGE */
-                    $event->addOrderPdfGeneration($orderPdfGenerationModel);
                 }
-            }
-        );
+            );
+        }
     }
 }
